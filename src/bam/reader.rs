32c2a31f6b1e90337c014875bf1d05543c37cf63
Julian Dicken <julian@dicken.de>
2024-11-25 18:29:59 +0100

    test

--- src/bam/reader.rs
+++ src/bam/reader.rs
@@ -1,9 +1,15 @@
+use anyhow::Result;
 use rust_htslib::bam;
 use rust_htslib::bam::Read;
-use anyhow::Result;
 use std::collections::HashSet;
 
-pub struct Reader {}
+pub struct Chunk {
+    start: 
+}
+
+pub struct Reader {
+
+}
 
 impl Reader {
     pub fn new() -> Self {
@@ -15,17 +21,18 @@ impl Reader {
         P: AsRef<std::path::Path>,
     {
         let mut bam = bam::Reader::from_path(file)?;
-        let names: Vec<_> = bam.header().target_names().iter().map(|n| String::from_utf8_lossy(n).to_string()).collect();
+        // not important right now
+        // let names: Vec<_> = bam
+        //     .header()
+        //     .target_names()
+        //     .iter()
+        //     .map(|n| String::from_utf8_lossy(n).to_string())
+        //     .collect();
+
         let mut record = bam::Record::new();
-        let mut seen = HashSet::new();
 
         while let Some(Ok(())) = bam.read(&mut record) {
-            if record.tid() >= 0 {
-                let target_name = &names[record.tid() as usize];
-                if seen.insert(target_name.to_string()) {
-                    println!("Reference: {}", target_name);
-                }
-            }
+            println!(record.tid())
         }
         Ok(())
     }
