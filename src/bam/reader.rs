d9168b5ed717bef0977ca1bd946f892f0ae8b1e0
Julian Dicken <julian@dicken.de>
2024-11-25 17:57:37 +0100

    working kinda

--- src/bam/reader.rs
+++ src/bam/reader.rs
@@ -10,24 +10,23 @@ impl Reader {
         Self {}
     }
 
-    pub fn create_cell_index<P>(file: P, out: P) -> Result<()> 
+    pub fn create_cell_index<P>(file: P, out: P) -> Result<()>
     where
-        P: AsRef<std::path::Path>
+        P: AsRef<std::path::Path>,
     {
         let mut bam = bam::Reader::from_path(file)?;
-        let names = bam.header().target_names();
+        let names: Vec<_> = bam.header().target_names().iter().map(|n| String::from_utf8_lossy(n).to_string()).collect();
         let mut record = bam::Record::new();
         let mut seen = HashSet::new();
-        
-        while let Some(()) = bam.read(&mut record)? {
+
+        while let Some(Ok(())) = bam.read(&mut record) {
             if record.tid() >= 0 {
-                let target_name = String::from_utf8_lossy(&names[record.tid() as usize]);
+                let target_name = &names[record.tid() as usize];
                 if seen.insert(target_name.to_string()) {
                     println!("Reference: {}", target_name);
                 }
             }
         }
-
         Ok(())
     }
 }
