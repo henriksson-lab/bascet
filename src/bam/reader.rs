4a4522bd52a5d17fc70c548bb06bd9b9e98479fc
Julian Dicken <julian@dicken.de>
2024-11-27 10:49:32 +0100

    working kinda

--- src/bam/reader.rs
+++ src/bam/reader.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use rust_htslib::bam;
+use rust_htslib::bam::{self, Read, record::Aux};
 use std::collections::HashSet;
 
 // pub struct Chunk {
@@ -9,19 +9,27 @@ use std::collections::HashSet;
 pub struct Reader {
 
 }
-
 impl Reader {
     pub fn new() -> Self {
         Self {}
     }
+
     pub fn create_cell_index<P>(file: P, out: P) -> Result<()>
     where
         P: AsRef<std::path::Path>,
     {
         let mut bam = bam::Reader::from_path(file)?;
         let mut record = bam::Record::new();
+        let mut seen_barcodes = HashSet::new();
+
         while let Some(Ok(())) = bam.read(&mut record) {
-            println!("{}", record.tid())
+            if let Ok(aux) = record.aux(b"CB") {
+                if let bam::record::Aux::String(cb_str) = aux {
+                    if seen_barcodes.insert(cb_str.to_string()) {
+                        println!("{}", cb_str);
+                    }
+                }
+            }
         }
         Ok(())
     }
