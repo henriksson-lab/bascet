4ac20df4bb10a3b590e46280e3b4775b8e32b386
Julian Dicken <julian@dicken.de>
2024-11-27 10:30:16 +0100

    sync

--- src/bam/reader.rs
+++ src/bam/reader.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use rust_htslib::cram;
+use rust_htslib::bam;
 use std::collections::HashSet;
 
 // pub struct Chunk {
@@ -14,17 +14,15 @@ impl Reader {
     pub fn new() -> Self {
         Self {}
     }
-
     pub fn create_cell_index<P>(file: P, out: P) -> Result<()>
     where
         P: AsRef<std::path::Path>,
     {
-        let mut cram = cram::Reader::from_path(file)?;
-        let mut record = cram::Record::new();
-        while let Some(Ok(())) = cram.read(&mut record) {
+        let mut bam = bam::Reader::from_path(file)?;
+        let mut record = bam::Record::new();
+        while let Some(Ok(())) = bam.read(&mut record) {
             println!("{}", record.tid())
         }
-
         Ok(())
     }
 }
