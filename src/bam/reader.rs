57cdd8fbec840fc45c4f77ea6ce17b47dbd4bc84
Julian Dicken <julian@dicken.de>
2024-12-06 12:59:23 +0100

    Tar ball working  i think

--- src/bam/reader.rs
+++ src/bam/reader.rs
@@ -3,9 +3,11 @@ use crossbeam::channel::{bounded, Sender};
 use regex::Regex;
 use rust_htslib::bam::{record::Aux, Read};
 use threadpool::ThreadPool;
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::{BufWriter, Write};
-use std::sync::LazyLock;
+use std::path::Path;
+use std::process::Command;
+use std::sync::{Arc, LazyLock, Mutex};
 
 #[derive(Clone)]
 struct CBBatch {
@@ -74,23 +76,67 @@ impl Reader {
 
         let write_pool = ThreadPool::new(n_write_threads);
         
+        let tar_path = Path::new("./data/temp/output.tar");
+        let _ = fs::create_dir_all(tar_path.parent().unwrap());
+        let tar_file = File::create(tar_path).expect("Failed to create tar file");
+        let builder = Arc::new(Mutex::new(tar::Builder::new(tar_file)));
+
         while let Ok(batch) = rx.recv() {
+            let builder = Arc::clone(&builder);
             write_pool.execute(move || {
                 let cb = String::from_utf8_lossy(&batch.cb);
                 if CB_PATTERN.is_match(&cb) && batch.sequences.len() > 1000 {
-                    let filename = format!("./data/temp/{}.fasta", cb);
-                    if let Ok(file) = File::create(filename) {
+                    // No idea why &* is necessary
+                    let temp_path = Path::new("./data/temp").join(&*cb);
+                    let _ = fs::create_dir_all(&temp_path);
+                    let reads_path = temp_path.join("reads.fastq");
+                    
+                    if let Ok(file) = File::create(&reads_path) {
                         let mut writer = BufWriter::new(file);
-                        
                         for (i, seq) in batch.sequences.iter().enumerate() {
-                            let _ = writeln!(writer, ">{}::{}", cb, i);
+                            let _ = writeln!(writer, "@{}::{}", cb, i);
                             let _ = writeln!(writer, "{}", String::from_utf8_lossy(seq));
+                            let _ = writeln!(writer, "+\n{}", String::from_utf8(vec![b'F'; seq.len()]).unwrap());
                         }
-                       
                         let _ = writer.flush();
+                        
+                        let spades_out_dir = temp_path.join("spades_out");
+                        let _ = fs::create_dir_all(&spades_out_dir);
+                        
+                        let status = Command::new("spades.py")
+                            .arg("-s").arg(&reads_path)
+                            .arg("--isolate")
+                            .arg("-o").arg(&spades_out_dir)
+                            .status()
+                            .expect("Failed to execute SPAdes");
+                        
+                        if status.success() {
+                            let contigs_path = spades_out_dir.join("contigs.fasta");
+                            if let Ok(mut builder) = builder.lock() {
+                                if let Err(e) = builder.append_path_with_name(
+                                    &contigs_path,
+                                    format!("{}/contigs.fasta", cb)
+                                ) {
+                                    eprintln!("Failed to add contigs to tar: {}", e);
+                                }
+                                if let Err(e) = builder.append_path_with_name(
+                                    &reads_path,
+                                    format!("{}/reads.fastq", cb)
+                                ) {
+                                    eprintln!("Failed to add contigs to tar: {}", e);
+                                }
+                            }
+                            
+                            let _ = fs::remove_dir_all(&temp_path);
+                        }
                     }
                 }
             });
+        }
+        if let Ok(mut builder) = builder.lock() {
+            if let Err(e) = builder.finish() {
+                eprintln!("Failed to finish tar file: {}", e);
+            }
         }
 
         write_pool.join();
