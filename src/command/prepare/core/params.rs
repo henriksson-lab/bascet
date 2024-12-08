84792e72216731e6208d6df1164aa5388936f40e
Julian Dicken <julian@dicken.de>
2024-12-08 16:03:06 +0100

    sync

--- src/command/prepare/core/params.rs
+++ src/command/prepare/core/params.rs
@@ -1,11 +1,17 @@
 pub struct IO<'a> {
-    pub file_in: &'a std::path::PathBuf,
+    pub path_in: &'a std::path::PathBuf,
+    pub path_temp: &'a std::path::PathBuf,
     pub path_out: &'a mut clio::Output,
 }
 
-pub struct Runtime {}
+pub struct Runtime {
+    pub min_reads: usize,
+}
 
-pub struct Threading {
+pub struct Threading<'a> {
     pub threads_read: u32,
     pub threads_write: usize,
+
+    pub thread_pool_read: &'a rust_htslib::tpool::ThreadPool,
+    pub thread_pool_write: &'a threadpool::ThreadPool,
 }
