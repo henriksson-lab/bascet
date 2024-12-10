5d8e3d5a4981665efbb0e1cebf889ff2eba89313
Julian Dicken <julian@dicken.de>
2024-12-10 15:58:38 +0100

    lock free splitting

--- src/command/prepare/core/params.rs
+++ src/command/prepare/core/params.rs
@@ -1,3 +1,5 @@
+use super::threading::ThreadState;
+
 pub struct IO {
     pub path_in: std::path::PathBuf,
     pub path_tmp: std::path::PathBuf,
@@ -10,9 +12,13 @@ pub struct Runtime {
     pub min_reads: usize,
 }
 
-pub struct Threading<'a> {
+pub struct Threading<'a, W>
+where
+    W: std::io::Seek + std::io::Write,
+{
     pub threads_write: usize,
     pub threads_read: u32,
     pub thread_pool_write: &'a threadpool::ThreadPool,
     pub thread_pool_read: &'a rust_htslib::tpool::ThreadPool,
+    pub thread_states: &'a Vec<std::sync::Arc<ThreadState<W>>>,
 }
---
+++ src/command/prepare/core/threading.rs
@@ -0,0 +1,23 @@
+pub struct ThreadState<W>
+where
+    W: std::io::Seek + std::io::Write,
+{
+    pub zip_writer: std::cell::UnsafeCell<zip::ZipWriter<std::io::BufWriter<W>>>,
+}
+
+unsafe impl<W> Send for ThreadState<W> where W: std::io::Seek + std::io::Write {}
+unsafe impl<W> Sync for ThreadState<W> where W: std::io::Seek + std::io::Write {}
+pub type DefaultThreadState = ThreadState<std::fs::File>;
+
+impl<W> ThreadState<W>
+where
+    W: std::io::Seek + std::io::Write,
+{
+    pub fn new(writer: W) -> Self {
+        Self {
+            zip_writer: std::cell::UnsafeCell::new(zip::ZipWriter::new(std::io::BufWriter::new(
+                writer,
+            ))),
+        }
+    }
+}
