13f2d818e912027f44e721422816bd856d518c19
Julian Dicken <julian@dicken.de>
2024-12-28 20:03:19 +0100

    Output dir changed to out to avoid conflicts

--- src/command/assemble/constants.rs
+++ src/command/assemble/constants.rs
@@ -1,6 +1,6 @@
-pub const ASSEMBLE_DEFAULT_PATH_IN: &str = "robert/reads.rdb";
-pub const ASSEMBLE_DEFAULT_PATH_TEMP: &str = "robert/temp";
-pub const ASSEMBLE_DEFAULT_PATH_OUT: &str = "robert/assemblies.rdb";
+pub const ASSEMBLE_DEFAULT_PATH_IN: &str = "out/reads.rdb";
+pub const ASSEMBLE_DEFAULT_PATH_TEMP: &str = "out/temp";
+pub const ASSEMBLE_DEFAULT_PATH_OUT: &str = "out/assemblies.rdb";
 pub const ASSEMBLE_DEFAULT_THREADS_READ: usize = 1;
 pub const ASSEMBLE_DEFAULT_THREADS_WRITE: usize = 5;
 pub const ASSEMBLE_DEFAULT_THREADS_WORK: usize = 2;
