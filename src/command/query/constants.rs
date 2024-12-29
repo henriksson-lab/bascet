c3b1194f3fb2288549a5425f74753d2d042d99ba
Julian Dicken <julian@dicken.de>
2024-12-29 19:53:39 +0100

    API refactor complete, I think.

--- src/command/query/constants.rs
+++ src/command/query/constants.rs
@@ -1,6 +1,8 @@
-pub const QUERY_DEFAULT_PATH_IN: &str = "";
-pub const QUERY_DEFAULT_PATH_TEMP: &str = "";
-pub const QUERY_DEFAULT_PATH_OUT: &str = "";
-pub const QUERY_DEFAULT_PATH_REF: &str = "";
+pub const QUERY_DEFAULT_PATH_IN: &str = "out/counts.rdb";
+pub const QUERY_DEFAULT_PATH_TEMP: &str = "out/temp";
+pub const QUERY_DEFAULT_PATH_OUT: &str = "out/query.mm";
+pub const QUERY_DEFAULT_PATH_REF: &str = "out/features.txt";
+pub const QUERY_DEFAULT_THREADS_READ: usize = 1;
+pub const QUERY_DEFAULT_THREADS_WORK: usize = 11;
 pub const QUERY_DEFAULT_FEATURES_MIN: usize = 10_0000;
 pub const QUERY_DEFAULT_FEATURES_MAX: usize = 1_000;
--- src/core/params.rs
+++ src/core/params.rs
@@ -1,6 +1,5 @@
 pub struct IO {
     pub path_in: std::path::PathBuf,
-    pub path_out: std::path::PathBuf,
 }
 pub struct Runtime {
     pub kmer_size: usize,
