df4e252179b8cce29a7acb8d60bdd67177718839
Johan Henriksson <mahogny@areta.org>
2025-01-02 15:04:34 +0100

    separated barcode

--- src/command/getraw/core/io.rs
+++ src/command/getraw/core/io.rs
@@ -23,48 +23,7 @@ use niffler::get_reader;
 use seq_io::fasta::{Reader as FastaReader, Record as FastaRecord};
 use seq_io::fastq::{Reader as FastqReader, Record as FastqRecord};
 
-use bio::alignment::Alignment;
-use bio::pattern_matching::myers::Myers;
 
-
-#[derive(Clone, Debug)]
-pub struct Barcode {
-    pub index: usize,
-    pub name: String,
-    pub pool: u32,
-    pub sequence: Vec<u8>,
-    pub pattern: Myers<u64>,
-}
-impl Barcode {
-    pub fn seek( ////////////// why mutable??
-        &mut self,
-//        &self,
-        record: &[u8],
-        distance: u8,
-    ) -> Vec<(&String, u32, Vec<u8>, usize, usize, i32)> {
-        // use Myers' algorithm to find the barcodes in a read
-        // Ref: Myers, G. (1999). A fast bit-vector algorithm for approximate string
-        // matching based on dynamic programming. Journal of the ACM (JACM) 46, 395–415.
-        let mut hits: Vec<(&String, u32, Vec<u8>, usize, usize, i32)> = Vec::new();
-        let mut aln = Alignment::default();
-        let mut matches = self.pattern.find_all_lazy(record, distance);  //^^^^^^^^^^^^ `self` is a `&` reference, so the data it refers to cannot be borrowed as mutable
-        let maybe_matches = matches.by_ref().min_set_by_key(|&(_, dist)| dist);
-        if maybe_matches.len() > 0 {
-            for (best_end, _) in maybe_matches {
-                matches.alignment_at(best_end, &mut aln);
-                hits.push((
-                    &self.name,
-                    self.pool,
-                    self.sequence.to_owned(),
-                    aln.ystart,
-                    aln.yend,
-                    aln.score,
-                ));
-            }
-        }
-        hits
-    }
-}
 
 pub struct BGZFFastqReader {
     pub reader: noodles_bgzf::Reader<File>,
@@ -222,36 +181,6 @@ pub fn open_cram(file_handle: &PathBuf) -> cram::io::Reader<File> {
     file
 }
 
-pub fn read_barcodes(barcode_files: &Vec<PathBuf>) -> Vec<Barcode> {
-    let mut barcodes: Vec<Barcode> = Vec::new();
-    for barcode_file in barcode_files {
-        let mut reader = open_fasta(barcode_file); // all barcodes should be in tsv files
-                                                   // open barcode file
-                                                   // tsv with the following columns (optional in parantheses):
-                                                   // pos	(well)	seq
-                                                   // let mut reader = File::open(barcode_file).unwrap();
-                                                   // buffer and iterator
-        let mut n_barcodes: usize = 0;
-        while let Some(record) = reader.next() {
-            let record = record.expect("Error reading record");
-            let b = Barcode {
-                index: n_barcodes,
-                name: record.id().unwrap().to_string(),
-                pool: 0,
-                sequence: record.seq().to_vec(),
-                pattern: Myers::<u64>::new(record.seq().to_vec()),
-            };
-            barcodes.push(b);
-            n_barcodes += 1;
-        }
-    }
-    // TODO check the edit distance between barcodes
-    info!(
-        "Found {} barcodes in specified barcode files",
-        barcodes.iter().count()
-    );
-    barcodes
-}
 
 pub fn open_buffer_for_writing(path: &PathBuf, append: bool, fail_on_exists: bool) -> File {
     let buffer = OpenOptions::new()
--- src/command/getraw/core/mod.rs
+++ src/command/getraw/core/mod.rs
@@ -4,3 +4,4 @@ pub mod params;
 pub mod index;
 pub mod io;
 pub mod stats;
+pub mod barcode;
