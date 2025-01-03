598ac476219f3c484343d77b2beba528a85615ea
Johan Henriksson <mahogny@areta.org>
2025-01-03 13:28:32 +0100

    cleanup

--- src/command/getraw/core/io.rs
+++ src/command/getraw/core/io.rs
@@ -1,6 +1,5 @@
 // This file is part of babbles which is released under the MIT license.
 // See file LICENSE or go to https://github.com/HadrienG/babbles for full license details.
-use itertools::Itertools;
 use log::{debug, error};
 use std::fs::{File, OpenOptions};
 use std::io::{self, BufRead, Write};
@@ -9,170 +8,14 @@ use std::process;
 
 use bgzip::{write::BGZFMultiThreadWriter, write::BGZFWriter, BGZFError, Compression};
 
-use noodles::cram;
-use noodles::sam;
 use noodles_bgzf as bgzf;
 use noodles_bgzf::VirtualPosition;
 
-use noodles::sam::alignment::{
-    record::data::field::Tag,
-    record_buf::{data::field::Value, Data},
-};
-
 use niffler::get_reader;
-use seq_io::fasta::{Reader as FastaReader, Record as FastaRecord};
+use seq_io::fasta::Reader as FastaReader;
 use seq_io::fastq::{Reader as FastqReader, Record as FastqRecord};
 
 
-
-
-
-
-
-////////////// CRAM file version
-pub fn write_records_pair_to_cram(
-    header: &sam::Header,
-    cram_writer: &mut cram::io::Writer<File>,
-    forward: &impl seq_io::fastq::Record,
-    reverse: &impl seq_io::fastq::Record,
-    barcodes_hits: &Vec<String>
-) {
-    // create the forward record
-    let fname = forward
-        .id()
-        .unwrap()
-        .as_bytes()
-        .split_last_chunk::<2>() // we want to remove the paired identifiers /1 and /2
-        .unwrap();
-    let mut forward_tags = Data::default();
-    forward_tags.insert(Tag::CELL_BARCODE_ID, Value::from(barcodes_hits.join("-")));
-    let forward_builder = cram::record::Builder::default()
-        .set_name(fname.0)
-        .set_read_length(forward.seq().len())
-        .set_bases(noodles::sam::alignment::record_buf::Sequence::from(
-            forward.seq(),
-        ))
-        .set_quality_scores(noodles::sam::alignment::record_buf::QualityScores::from(
-            forward.qual().iter().map(|&n| n - 33).collect::<Vec<u8>>(),
-        ))
-        .set_flags(cram::record::Flags::from(0x07))
-        .set_bam_flags(noodles::sam::alignment::record::Flags::from(0x4D))
-        .set_tags(forward_tags);
-    let forward_record = forward_builder.build();
-
-    //NOW THE REVERSE
-    let rname = reverse
-        .id()
-        .unwrap()
-        .as_bytes()
-        .split_last_chunk::<2>() // we want to remove the paired identifiers /1 and /2
-        .unwrap(); // TODO assert names are same
-    let mut reverse_tags = Data::default();
-    reverse_tags.insert(Tag::CELL_BARCODE_ID, Value::from(barcodes_hits.join("-")));
-    let reverse_builder = cram::record::Builder::default()
-        .set_name(rname.0)
-        .set_read_length(reverse.seq().len())
-        .set_bases(noodles::sam::alignment::record_buf::Sequence::from(
-            reverse.seq(),
-        ))
-        .set_quality_scores(noodles::sam::alignment::record_buf::QualityScores::from(
-            reverse.qual().iter().map(|&n| n - 33).collect::<Vec<u8>>(),
-        ))
-        .set_flags(cram::record::Flags::from(0x03))
-        .set_bam_flags(noodles::sam::alignment::record::Flags::from(0x8D))
-        .set_tags(reverse_tags);
-    let reverse_record = reverse_builder.build();
-
-    // bam flags
-    // tags to find forward and reverse: (see samtools doc)
-    // >>> 64 + 1 + 4 + 8
-    // 77
-    // >>> 128 + 1 + 4 + 8
-    // 141
-
-    cram_writer
-        .write_record(&header, forward_record)
-        .expect("Failed to write read to cram");
-    cram_writer
-        .write_record(&header, reverse_record)
-        .expect("Failed to write read to cram");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* 
-
-pub fn create_cram_or_bam_file<W>(
-    name: &PathBuf
-) -> (sam::Header, Box<dyn GeneralWriter<W>>) {
-
-    //TODO
-
-
-    debug!("Creating CRAM/BAM file: {}", name.display());
-    let header = sam::Header::builder()
-        .set_header(Default::default()) // need a Map here
-        // to add other header felds with Map::other_fields_mut()
-        // or tag or program
-        .add_comment("babbles")
-        .build();
-
-    //Delete file if it exists
-    if std::fs::exists(name).expect("Cannot check if cram/bam file already exists") {
-        std::fs::remove_file(name).expect("Failing to remove target cram/bam file");
-    }
-
-    let out_buffer = open_buffer_for_writing(name, true, false);  //fail_on_exists makes rust just end. set to false because hard to debug
-
-    let mut writer = if name.ends_with("cram") {
-
-        let mut writer = cram::io::Writer::new(out_buffer);
-
-        writer
-            .write_header(&header)
-            .expect("Failed to write header to CRAM file");
-        (header, Box::new(writer))
-    
-
-    } else if name.ends_with("cram") {
-
-        let mut writer = bam::io::Writer::new(out_buffer);
-
-        writer
-            .write_header(&header)
-            .expect("Failed to write header to BAM file");
-
-        (header, Box::new(writer))
-    } else {
-        panic!("unsupported file format");
-    };
-    writer
-
-}
-
-
-
-
-*/
-
-
-
-
-//////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////
 
 pub struct BGZFFastqReader {
     pub reader: noodles_bgzf::Reader<File>,
@@ -246,6 +89,11 @@ impl BGZFRecord {
     }
 }
 
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+
 // "only traits defined in the current crate can be implemented for types
 // defined outside of the crate. Define and implement a trait or new type instead"
 // If we'd ever want to have all fastx IO with niffler + seqio ...
@@ -317,18 +165,6 @@ pub fn open_fastq_bgzipped(file_handle: &PathBuf) -> BGZFFastqReader {
     BGZFFastqReader { reader: file }
 }
 
-pub fn open_cram(file_handle: &PathBuf) -> cram::io::Reader<File> {
-    debug!("opening cram file: {}", file_handle.display());
-    let opened = match File::open(file_handle) {
-        Ok(file) => file,
-        Err(_) => {
-            error!("Could not open forward file {}", &file_handle.display());
-            process::exit(1)
-        }
-    };
-    let file = cram::io::Reader::new(opened);
-    file
-}
 
 
 pub fn open_buffer_for_writing(path: &PathBuf, append: bool, fail_on_exists: bool) -> File {
@@ -379,34 +215,6 @@ pub fn fastq_to_bgz(path: &PathBuf, output: &PathBuf) {
     }
     writer.close().unwrap();
 }
-
-
-
-
-pub fn create_cram_file(name: &PathBuf) -> (sam::Header, cram::io::Writer<File>) {
-    debug!("Creating CRAM file: {}", name.display());
-    let header = sam::Header::builder()
-        .set_header(Default::default()) // need a Map here
-        // to add other header felds with Map::other_fields_mut()
-        // or tag or program
-        .add_comment("babbles")
-        .build();
-
-    //Delete file if it exists
-    if std::fs::exists(name).expect("Cannot check if cram file already exists") {
-        std::fs::remove_file(name).expect("Failing to remove target cram file");
-    }
-
-    let out_buffer = open_buffer_for_writing(name, true, false);  //fail_on_exists makes rust just end. set to false because hard to debug
-    let mut writer = cram::io::Writer::new(out_buffer);
-
-    writer
-        .write_header(&header)
-        .expect("Failed to write header to CRAM file");
-    (header, writer)
-}
-
-
 
 
 
