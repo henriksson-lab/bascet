8f655c60a81048edfdde096cd250a07f28695db3
Johan Henriksson <mahogny@areta.org>
2025-04-08 21:20:38 +0200

    more loginfo

--- src/utils/merge_archives.rs
+++ src/utils/merge_archives.rs
@@ -37,13 +37,21 @@ where
     let mut bufwriter_destination = BufWriter::new(&file_destination);
     let mut zipwriter_destination = ZipWriter::new(&mut bufwriter_destination);
 
+    let mut num_out = 0;
     for source in sources {
+        println!("{}", source.as_ref().display());
         let file_source = File::open(&source).unwrap();
         let mut archive_source = ZipArchive::new(&file_source).unwrap();
 
         for i in 0..archive_source.len() {
+
+            if num_out%100 == 0 {
+                println!("{} {} / {} ", source.as_ref().display(), i, archive_source.len());
+            }
+
             let file = archive_source.by_index(i)?;
             zipwriter_destination.raw_copy_file(file)?;
+            num_out += 1;
         }
 
         fs::remove_file(source).unwrap();
