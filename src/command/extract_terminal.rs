4d37d48ef55b12d7908793df941053f6c98254e0
Johan Henriksson <mahogny@areta.org>
2025-04-17 19:17:37 +0200

    error message to terminal

--- src/command/extract_terminal.rs
+++ src/command/extract_terminal.rs
@@ -23,91 +23,96 @@ impl ExtractStreamCMD {
 
         //This is an interactive terminal to navigate Bascet-ZIP content; could generalize to any container later
 
-        //Tell version info etc. Final line in heady is always ready
+        //Tell version info etc. Final line is "ready" or "error"
         println!("version_major:1");
         println!("version_minor:0");
-        println!("ready");
+        let file = File::open(&self.path_in);
+        
+        if let Ok(file) = file {
+            println!("ready");
 
-        let file = File::open(&self.path_in).expect("Failed to open file");
-        let bufreader_shard = BufReader::new(file);
-        let mut zip_shard =    ZipArchive::new(bufreader_shard).unwrap();
-
-
-        loop {
-            let mut buffer = String::new();
-            let stdin = io::stdin();
-            stdin.read_line(&mut buffer)?;    
-            let buffer = buffer.trim();
-
-            if buffer=="help" {
-                println!("Available commands: exit ls showtext extract_to");
-                println!("Note that this system is optimized for streaming data to Zorn, and not for being user friendly to terminal users!");
-            } else if buffer=="ls" {
-                let list_files = zip_shard.file_names().collect_vec();
-                println!("{}", list_files.len());
-                for f in list_files {
-                    println!("{}", f);
-                }
-            } else if buffer.starts_with("showtext") {
-                //let fname=&buffer[b"showtext ".len()..];
-                let mut splitter=buffer.split_whitespace();
-                splitter.next();
-                let zip_entry_name = splitter.next().expect("Did not get zip entry name");
-
-
-                let entry = zip_shard.by_name(&zip_entry_name);
-                if let Ok(entry) = entry {
-                    if entry.is_file() {
-
-                        //Figure out how many lines there are in this file
-                        let reader = io::BufReader::new(entry);
-                        let mut lines: Vec<String> = Vec::new();
-                        for line in reader.lines().map_while(Result::ok) {
-                            lines.push(line);
-                        }
-
-                        //Print each line
-                        println!("{}", lines.len());
-                        for line in lines {
-                            println!("{}", line);
+            let bufreader_shard = BufReader::new(file);
+            let mut zip_shard =    ZipArchive::new(bufreader_shard).unwrap();
+    
+    
+            loop {
+                let mut buffer = String::new();
+                let stdin = io::stdin();
+                stdin.read_line(&mut buffer)?;    
+                let buffer = buffer.trim();
+    
+                if buffer=="help" {
+                    println!("Available commands: exit ls showtext extract_to");
+                    println!("Note that this system is optimized for streaming data to Zorn, and not for being user friendly to terminal users!");
+                } else if buffer=="ls" {
+                    let list_files = zip_shard.file_names().collect_vec();
+                    println!("{}", list_files.len());
+                    for f in list_files {
+                        println!("{}", f);
+                    }
+                } else if buffer.starts_with("showtext") {
+                    //let fname=&buffer[b"showtext ".len()..];
+                    let mut splitter=buffer.split_whitespace();
+                    splitter.next();
+                    let zip_entry_name = splitter.next().expect("Did not get zip entry name");
+    
+    
+                    let entry = zip_shard.by_name(&zip_entry_name);
+                    if let Ok(entry) = entry {
+                        if entry.is_file() {
+    
+                            //Figure out how many lines there are in this file
+                            let reader = io::BufReader::new(entry);
+                            let mut lines: Vec<String> = Vec::new();
+                            for line in reader.lines().map_while(Result::ok) {
+                                lines.push(line);
+                            }
+    
+                            //Print each line
+                            println!("{}", lines.len());
+                            for line in lines {
+                                println!("{}", line);
+                            }
+                        } else {
+                            println!("error not a file");
                         }
                     } else {
-                        println!("error not a file");
+                        println!("error missing -{}-", zip_entry_name);
                     }
-                } else {
-                    println!("error missing -{}-", zip_entry_name);
-                }
-
-            } else if buffer.starts_with("extract_to") {
-                let mut splitter=buffer.split_whitespace();
-                splitter.next();
-                let zip_entry_name = splitter.next().expect("Did not get zip entry name");
-                let path_outfile = splitter.next().expect("Did not get out file name");
-
-                //let fname=&buffer[b"showtext ".len()..];
-                let mut entry = zip_shard.by_name(&zip_entry_name);
-                if let Ok(entry) = &mut entry {
-                    if entry.is_file() {
-                        let file_out = File::create(&path_outfile).unwrap();
-                        let mut bufwriter_out = BufWriter::new(&file_out);
-                        let mut bufreader_found = BufReader::new(entry);
-                        std::io::copy(&mut bufreader_found, &mut bufwriter_out).unwrap();
-                        println!("done");
+    
+                } else if buffer.starts_with("extract_to") {
+                    let mut splitter=buffer.split_whitespace();
+                    splitter.next();
+                    let zip_entry_name = splitter.next().expect("Did not get zip entry name");
+                    let path_outfile = splitter.next().expect("Did not get out file name");
+    
+                    //let fname=&buffer[b"showtext ".len()..];
+                    let mut entry = zip_shard.by_name(&zip_entry_name);
+                    if let Ok(entry) = &mut entry {
+                        if entry.is_file() {
+                            let file_out = File::create(&path_outfile).unwrap();
+                            let mut bufwriter_out = BufWriter::new(&file_out);
+                            let mut bufreader_found = BufReader::new(entry);
+                            std::io::copy(&mut bufreader_found, &mut bufwriter_out).unwrap();
+                            println!("done");
+                        } else {
+                            println!("error not a file");
+                        }
                     } else {
-                        println!("error not a file");
+                        println!("error missing -{}-", zip_entry_name);
                     }
+    
+                } else if buffer=="exit" {
+                    break;
                 } else {
-                    println!("error missing -{}-", zip_entry_name);
+                    println!("error unknown command -{}-", buffer);
                 }
-
-            } else if buffer=="exit" {
-                break;
-            } else {
-                println!("error unknown command -{}-", buffer);
             }
-        }
-        println!("exiting");
+            println!("exiting");            
 
+        } else {
+            println!("error no such file {}", self.path_in);
+        }
         Ok(())
     }
 }
