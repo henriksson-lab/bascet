82c23afe8999b77d0698350d88389c5dfa8e739c
Johan Henriksson <mahogny@areta.org>
2025-04-22 16:43:03 +0200

    edits toward publishing package

--- src/command/countfeature.rs
+++ src/command/countfeature.rs
@@ -3,6 +3,7 @@ use std::io::BufReader;
 use std::path::PathBuf;
 use std::collections::HashMap;
 
+use bio::bio_types::strand::ReqStrand;
 use rust_htslib::bam::Read;
 use rust_htslib::bam::record::Record as BamRecord;
 
@@ -73,6 +74,7 @@ impl CountFeatureCMD {
 
 type Cellid = Vec<u8>;
 
+type ChromosomeID = Vec<u8>;
 
 
 pub enum BascetStrand {  //equivalent to GFF 
@@ -109,7 +111,7 @@ impl CountFeature {
         bam.set_threads(params.num_threads).unwrap();
 
         //Keep track of last chromosome seen (assuming that file is sorted)
-        let mut last_chr:Vec<u8> = Vec::new();        
+        //let mut last_chr:Vec<u8> = Vec::new();        
 
         //Map cellid -> count. Note that we do not have a list of cellid's at start; we need to harmonize this later
         //let mut map_cell_count: BTreeMap<Cellid, uint> = BTreeMap::new();
@@ -129,42 +131,26 @@ impl CountFeature {
                 let header = bam.header();
                 let chr = header.tid2name(record.tid() as u32);
 
-                //Check if we now work on a new chromosome
-                if chr!=last_chr {    
-    /* 
-                    //Store counts for this cell
-                    if !map_cell_count.is_empty() { //Only empty the first loop
-
-                        if !last_chr.is_empty() { //Do not store empty feature
-                            //TODO count this read
-                            let feature_index = cnt_mat.get_or_create_feature(&last_chr.to_vec());
-                            cnt_mat.add_cell_counts(
-                                feature_index,
-                                 &mut map_cell_count
-                            );
-                            
-                        }
-                        //println!("{:?}", map_cell_count);
-                        //Clear buffers, move to the next cell
-                        map_cell_count.clear();
-                    }
-                    */
-                    last_chr=chr.to_vec();
-                } 
-
                 //Figure out the cell barcode. In one format, this is before the first :
                 //TODO support read name as a TAG
                 let read_name = record.qname();
                 let mut splitter = read_name.split(|b| *b == b':'); 
                 let cell_id = splitter.next().expect("Could not parse cellID from read name");
+                let umi = splitter.next().expect("Could not parse UMI from read name");
 
-                /* 
-                //Count this read
-                let values = map_cell_count.entry(cell_id.to_vec()).or_insert(0);
-                *values += 1;*/
+               
+                let strand = match record.strand() {
+                    ReqStrand::Forward => Strand::Forward,
+                    ReqStrand::Reverse => Strand::Reverse
+                };
 
                 gff.count_read(
-                    cell_id
+                    cell_id,
+                    umi,
+                    chr,
+                    record.pos(), //or mpos? TODO
+                    record.cigar().end_pos(),
+                    strand
                 );
 
 
@@ -210,37 +196,66 @@ impl CountFeature {
 
 //// Counter of reads for one gene and cell
 pub struct CounterForCell {
-    pub umis: Vec<String>,
+    pub umis: Vec<Vec<u8>>,
+}
+impl CounterForCell {
+    fn new() -> CounterForCell {
+        CounterForCell {
+            umis: Vec::new()
+        }
+    }
 }
 
 
 
-//// Information about one gene
+//// Information about one gene, and collectors of reads from each cell
 pub struct GeneCounter {
-    pub gene_chr: String,
-    pub gene_start: usize,
-    pub gene_end: usize,
+    pub gene_chr: ChromosomeID,
+    pub gene_start: i64,
+    pub gene_end: i64,
     pub gene_strand: Strand,
 
     pub gene_id: String,
     pub gene_name: String,
 
-    pub counters: HashMap<String, CounterForCell>,
+    pub counters: HashMap<Cellid, CounterForCell>,
 }
 impl GeneCounter {
+    fn finalize(&mut self) {
+        //Don't send this gene for calculation if trivially empty
+        if self.counters.len() > 0 {
+
+            //For each cell:
+            for (cellid, counter) in self.counters.iter() {
 
 
+
+
+
+
+            }
+
+
+            //Perform UMI deduplication
+
+            //Count
+
+            //Send count to a writer
+
+
+
+            // https://github.com/sstadick/rumi  -- can use. wants htslib Record; keep all the way to the end?
+
+        }
+    }
 }
 
 
 
-//// Container of genes for one chromosome
+//// Container for genes, located on one chromosome
 pub struct GFFchrom {
     pub genes: Vec<GeneCounter>,
     pub current_pos: usize
-
-    //Or keep pointer to start, use a vec
-
 }
 impl GFFchrom {
 
@@ -260,7 +275,17 @@ impl GFFchrom {
 
 
     pub fn sort(&mut self) {
-        self.genes.sort_by_key(|e| e.gene_start);
+        self.genes.sort_by_key(|e| e.gene_start); /////////////////////////////// todo need to also sort by end position!!! currently there is a bug TODO
+    }
+
+    pub fn finish(&mut self) {
+        //For any remaining genes, wrap up
+        let mut cur_gene: usize = self.current_pos;
+        while cur_gene < self.genes.len() {
+            let this_gene = self.genes.get_mut(cur_gene).unwrap();
+            this_gene.finalize();
+            cur_gene += 1;
+        }
     }
 }
 
@@ -269,26 +294,76 @@ impl GFFchrom {
 
 //// Container of genes for all chromosomes
 pub struct GFF {
-    chroms: HashMap<String, GFFchrom>
-    //    pub genes: LinkedList<GeneCounter>
-    //Or keep pointer to start, use a vec    
+    chroms: HashMap<Vec<u8>, GFFchrom>,
+    last_chrom: Vec<u8>
 }
 impl GFF {
 
 
     pub fn new() -> GFF {
         GFF { 
-            chroms: HashMap::new()
+            chroms: HashMap::new(),
+            last_chrom: Vec::new()
         }
     }
 
 
     pub fn count_read(
         &mut self,
-        _cell_id: &[u8]
+        cell_id: &[u8],
+        umi: &[u8],
+        chr: &[u8],
+        start: i64,
+        end: i64,
+        _strand: Strand  //TOOD make use of this. chemistry dependent
     ) {
 
+        //If we moved to a new chromosome, ensure we wrap up the counters on the previous one
+        if chr!=self.last_chrom {    
+            let prev = self.chroms.get_mut(&self.last_chrom);
+            if let Some(prev) = prev {
+                prev.finish();
+            }
+            self.last_chrom = chr.to_vec();
+        }
+
+        //Investigate relevant chromosome
+        let gff_chrom = self.chroms.get_mut(chr);
+        if let Some(gff_chrom) = gff_chrom {
+            //Loop over all genes on this chromosome
+            let mut cur_gene = gff_chrom.current_pos;
+            while cur_gene < gff_chrom.genes.len() {
+                let this_gene = gff_chrom.genes.get_mut(cur_gene).unwrap();
+
+                //See if the read overlaps current gene
+                if this_gene.gene_end < start {
+                    //We are past this gene. The counting can be finalized
+                    this_gene.finalize();
+                    gff_chrom.current_pos += 1;
+                } else if end < this_gene.gene_start {
+                    //This gene is beyond the current read. Since reads are sorted,
+                    //we need not check more genes
+                    break;
+                }
+
+                //This gene overlaps, so add the read count
+                let counter = this_gene.counters.entry(cell_id.into()).or_insert(CounterForCell::new());
+                counter.umis.push(umi.into());
+
+                //Proceed to check the next gene
+                cur_gene += 1;
+            }
+
+
+
+
+
+        } else {
+            println!("Read from chromosome not declared in GFF; ignoring: {}", String::from_utf8(chr.into()).unwrap());
+        }
     }
+
+
 
 
     pub fn add_feature(
@@ -355,9 +430,9 @@ impl GFF {
                     };
 
                     let gc = GeneCounter {
-                        gene_chr: record.reference_sequence_name().to_string(),
-                        gene_start: record.start().into(),
-                        gene_end: record.end().into(),
+                        gene_chr: record.reference_sequence_name().to_vec(),
+                        gene_start: record.start().get() as i64,
+                        gene_end: record.end().get() as i64,
                         gene_strand: record.strand(),
             
                         gene_id: attr_id,
---
+++ src/utils/umi_dedup.rs
@@ -0,0 +1,24 @@
+
+//inspiration
+//https://github.com/sstadick/rumi/blob/master/src/lib.rs
+
+//suggests a BK-tree https://en.wikipedia.org/wiki/BK-tree  (not in above code)
+
+
+// use basebits::{hamming_dist_none, BaseBits};  interesting lib!
+// https://github.com/sstadick/basebits
+// same guy
+// https://www.biorxiv.org/content/10.1101/648683v1.full
+// constant time hamming distance
+// at biorad https://github.com/sstadick  
+
+
+// https://peerj.com/articles/8275/
+
+// could use this also to scan for barcodes if errors are present
+
+
+
+// there are several bk-tree implementations in rust:
+// https://www.google.com/search?q=rust+bk-tree&oq=rust+bk-tree&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIJCAEQABgNGIAEMggIAhAAGA0YHjIICAMQABgNGB4yCggEEAAYCBgNGB4yCggFEAAYCBgNGB4yDQgGEAAYhgMYgAQYigUyDQgHEAAYhgMYgAQYigUyDQgIEAAYhgMYgAQYigXSAQgxOTkxajBqN6gCALACAA&sourceid=chrome&ie=UTF-8
+
