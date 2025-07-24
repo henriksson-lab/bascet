bfef4bf8dd49c13b5db68a0af639eb7c421904f5
Julian Dicken <julian@dicken.de>
2025-07-24 15:30:31 +0200

    going to refactor for rayon-compatible api and then clean up

--- src/io/format/TIRP/parse.rs
+++ src/io/format/TIRP/parse.rs
@@ -4,13 +4,29 @@ use crate::{
 };
 
 pub fn parse_readpair(buf_record: &[u8]) -> Result<ReadPair, format::Error> {
-    let parts: Vec<&[u8]> = buf_record.split(|&b| b == common::U8_CHAR_TAB).collect();
-    let id = parts[0];
-    let r1 = parts[3];
-    let r2 = parts[4];
-    let q1 = parts[5];
-    let q2 = parts[6];
-    let umi = parts[7];
+    
+    let mut start = 0;
+    let mut tab_iter = memchr::memchr_iter(common::U8_CHAR_TAB, buf_record);
+
+    let tab0 = tab_iter.next().unwrap();
+    let id = &buf_record[0..tab0];
+
+    // Skip to tab 2 (we need parts[3])
+    let _tab1 = tab_iter.next().unwrap();
+    let _tab2 = tab_iter.next().unwrap();
+    let tab3 = tab_iter.next().unwrap();
+    let r1 = &buf_record[_tab2 + 1..tab3];
+
+    let tab4 = tab_iter.next().unwrap();
+    let r2 = &buf_record[tab3 + 1..tab4];
+
+    let tab5 = tab_iter.next().unwrap();
+    let q1 = &buf_record[tab4 + 1..tab5];
+
+    let tab6 = tab_iter.next().unwrap();
+    let q2 = &buf_record[tab5 + 1..tab6];
+
+    let umi = &buf_record[tab6 + 1..];
 
     if r1.len() != q1.len() {
         return Err(format::Error::ParseError {
