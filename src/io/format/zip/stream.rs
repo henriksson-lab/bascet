a774be318cf04b5807185a539c1e26ddbde49150
Julian Dicken <julian@dicken.de>
2025-07-25 23:37:12 +0200

    +feat zip over assemblies (cleanup required later)

--- src/io/format/zip/stream.rs
+++ src/io/format/zip/stream.rs
@@ -10,7 +10,7 @@ use zip::{HasZipMetadata, ZipArchive};
 
 use crate::{
     common::{self},
-    io::{self, AutoCountSketchStream, BascetFile, BascetStream, BascetStreamToken},
+    io::{self, BascetFile, BascetStream, BascetStreamToken, BascetStreamTokenBuilder},
     log_critical, log_info,
 };
 
@@ -18,8 +18,6 @@ pub struct Stream<T> {
     inner_archive: ZipArchive<std::fs::File>,
     inner_files: Vec<String>,
     inner_files_cursor: usize,
-    inner_buf: Vec<u8>,
-    inner_cursor: usize,
 
     worker_threadpool: threadpool::ThreadPool,
 
@@ -41,8 +39,6 @@ impl<T> Stream<T> {
             inner_archive: archive,
             inner_files: files,
             inner_files_cursor: 0,
-            inner_buf: vec![0u8; 0],
-            inner_cursor: 0,
 
             worker_threadpool: threadpool::ThreadPool::new(1),
 
@@ -61,7 +57,7 @@ impl<T> BascetStream<T> for Stream<T>
 where
     T: BascetStreamToken + Send + 'static,
 {
-    fn next(&mut self) -> anyhow::Result<Option<T>> {
+    fn next_cell(&mut self) -> anyhow::Result<Option<T>> {
         let archive = &mut self.inner_archive;
 
         if self.inner_files_cursor >= self.inner_files.len() {
@@ -74,10 +70,7 @@ where
             .unwrap();
 
         self.inner_files_cursor += 1;
-        self.inner_buf.clear(); // Prevent memory growth
-        self.inner_cursor = 0;
 
-        let mut reads = Vec::<Vec<u8>>::with_capacity(1000);
         let path = file.get_metadata().file_name_sanitized();
         let id = path
             .parent()
@@ -86,44 +79,40 @@ where
             .unwrap()
             .as_encoded_bytes();
 
-        if let Ok(bytes_read) = file.read_to_end(&mut self.inner_buf) {
+        let mut builder = T::builder().add_cell_id_owned(id.to_vec());
+        let mut cursor = 0;
+        let mut buffer: Vec<u8> = Vec::new();
+
+        if let Ok(bytes_read) = file.read_to_end(&mut buffer) {
             match bytes_read {
                 0 => {
-                    let token = T::new(id, reads);
+                    let token = builder.build();
                     return Ok(Some(token));
                 }
                 _ => {
-                    while let Some(next_pos) = memchr::memchr(
-                        common::U8_CHAR_FASTA_IDEN,
-                        &self.inner_buf[self.inner_cursor..],
-                    ) {
+                    while let Some(next_pos) =
+                        memchr::memchr(common::U8_CHAR_FASTA_IDEN, &buffer[cursor..])
+                    {
                         // next record exists
-                        let line = match memchr::memchr(
-                            common::U8_CHAR_FASTA_IDEN,
-                            &self.inner_buf[self.inner_cursor..],
-                        ) {
-                            Some(eor) => {
-                                &self.inner_buf
-                                    [self.inner_cursor..(self.inner_cursor + eor).saturating_sub(1)]
-                            }
-                            None => &self.inner_buf[self.inner_cursor..],
-                        };
+                        let line =
+                            match memchr::memchr(common::U8_CHAR_FASTA_IDEN, &buffer[cursor..]) {
+                                Some(eor) => &buffer[cursor..(cursor + eor).saturating_sub(1)],
+                                None => &buffer[cursor..],
+                            };
 
                         let seq = match memchr::memchr(common::U8_CHAR_NEWLINE, line) {
                             Some(record_seq_start) => &line[record_seq_start + 1..],
                             None => {
-                                self.inner_cursor += next_pos + 1;
+                                cursor += next_pos + 1;
 
                                 continue;
                             }
                         };
-
-                        reads.push(seq.to_vec());
-
-                        self.inner_cursor += next_pos + 1;
+                        builder = builder.add_sequence_owned(seq.to_vec());
+                        cursor += next_pos + 1;
                     }
 
-                    let token = T::new(id, reads);
+                    let token = builder.build();
                     return Ok(Some(token));
                 }
             }
@@ -131,77 +120,7 @@ where
         Err(anyhow::anyhow!("Read error"))
     }
 
-    fn par_map<F, R, G, L>(
-        &mut self,
-        global_state: G,
-        local_states: Vec<L>,
-        f: F,
-    ) -> (Vec<R>, Arc<G>, Vec<L>)
-    where
-        F: Fn(T, &G, &mut L) -> R + Send + Sync + 'static,
-        R: Send + 'static,
-        G: Send + Sync + 'static,
-        L: Send + 'static,
-    {
-        let n_workers = self.worker_threadpool.max_count();
-        let (wtx, wrx) = crossbeam::channel::bounded::<Option<T>>(128);
-        let (rtx, rrx) = crossbeam::channel::bounded::<(Vec<R>, L)>(n_workers);
-
-        let global_state = Arc::new(global_state);
-        let mut local_states = local_states.into_iter();
-
-        let f = Arc::new(f);
-        for _ in 0..n_workers {
-            let rx = wrx.clone();
-            let rtx = rtx.clone();
-
-            let f = Arc::clone(&f);
-            let g = Arc::clone(&global_state);
-
-            let mut local_state = match local_states.next() {
-                Some(state) => state,
-                None => panic!("no local state available"),
-            };
-
-            self.worker_threadpool.execute(move || {
-                let mut thread_results = Vec::new();
-                while let Ok(Some(token)) = rx.recv() {
-                    let result = f(token, g.as_ref(), &mut local_state);
-                    thread_results.push(result);
-                }
-                let _ = rtx.send((thread_results, local_state));
-            });
-        }
-
-        // Feed tokens to workers
-        while let Ok(Some(token)) = self.next() {
-            let _ = wtx.send(Some(token));
-        }
-
-        // Signal workers to stop
-        for _ in 0..n_workers {
-            let _ = wtx.send(None);
-        }
-
-        self.worker_threadpool.join();
-
-        let mut results = Vec::new();
-        let mut local_states = Vec::new();
-        for _ in 0..n_workers {
-            if let Ok((mut thread_vec, local_state)) = rrx.recv() {
-                results.append(&mut thread_vec);
-                local_states.push(local_state);
-            }
-        }
-
-        (results, global_state, local_states)
-    }
-
-    fn set_reader_threads(&mut self, n_threads: usize) {
-        // todo!();
-    }
-
-    fn set_worker_threads(&mut self, n_threads: usize) {
-        self.worker_threadpool.set_num_threads(n_threads);
+    fn set_reader_threads(mut self, n_threads: usize) -> Self {
+        self
     }
 }
