a5ab637d520005d2b45c9ddbce71867def05749f
Julian Dicken <julian@dicken.de>
2025-09-02 08:37:54 +0200

    merge perf-shardify

--- src/io/format/tirp/mod.rs
+++ src/io/format/tirp/mod.rs
@@ -1,13 +1,9 @@
-// pub mod extract;
 pub mod file;
 pub mod parse;
-// pub mod reader;
 pub mod stream;
 pub mod writer;
 
-// pub use extract::*;
 pub use file::*;
 pub use parse::*;
-// pub use reader::*;
 pub use stream::*;
 pub use writer::*;
