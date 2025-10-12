357fe6ba8947752632bb53b27e7b9fd175e50e1f
Julian Dicken <julian@dicken.de>
2025-10-12 14:57:51 +0200

    hope that fixes all warnings?

--- src/io/traits/read.rs
+++ src/io/traits/read.rs
@@ -1,10 +1,10 @@
-pub trait BascetRead {
-    // Check if a cell exists.
-    fn has_cell(&self, cell: &str) -> bool;
+// pub trait BascetRead {
+//     // Check if a cell exists.
+//     fn has_cell(&self, cell: &str) -> bool;
 
-    // List all cell IDs.
-    fn get_cells(&self) -> Vec<String>;
+//     // List all cell IDs.
+//     fn get_cells(&self) -> Vec<String>;
 
-    // Retrieve all records for a cell.
-    fn read_cell(&mut self, cell: &str) -> Vec<crate::common::ReadPair>;
-}
+//     // Retrieve all records for a cell.
+//     fn read_cell(&'_ mut self, cell: &str) -> Vec<crate::common::ReadPair<'_>>;
+// }
