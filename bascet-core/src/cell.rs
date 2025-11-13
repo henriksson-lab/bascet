0d2139a95ad163921cafc58c3ca8869847dfd3ca
Julian Dicken <julian@dicken.de>
2025-11-13 15:43:59 +0100

    .

--- bascet-core/src/cell.rs
+++ bascet-core/src/cell.rs
@@ -1,7 +1,6 @@
 //! Cell API for building and accessing structured data.
 //!
 //! # Example
-//!
 //! ```
 //! use bascet_core::cell::*;
 //! use bascet_provide::*;
@@ -26,12 +25,36 @@
 //! let (id, read) = cell.get_ref::<(Id, Read)>();
 //! assert_eq!(id, b"cell_test_modified");
 //! ```
+//! In addition, several flags exist to modify builder and provider:
+//! ```
+//! // nobuild flag creates a noop build with an unreachable-flagged getter
+//! #[cell(Id, Read, Metadata(nobuild: &'static str))]
+//! pub struct TestCell {
+//!     // example of a build override
+//!     #[with(|builder, value| builder.id = value.into_iter().map(|v| v * 2).collect(); builder)]
+//!     id: Vec<u8>,
+//!
+//!     // example of default override
+//!     #[default(|| vec!["ATGCATCC".into()])]
+//!     read: Vec<Vec<u8>>,
+//! }
+//! let mut cell = TestCell::builder()
+//!     .with::<Id>(vec![1, 2, 3])
+//!     .with::<Metadata>("This will be ignored")
+//!     .build();
+//! let (id, read) = cell.get_ref::<(Id, Read)>();
+//! assert_eq!(id, vec![2, 4, 6]);
+//! assert_eq!(id, vec![b"ATGCATCC".to_vec()]);
+//!
+//! // does not compile:
+//! let (metadata) = cell.get_ref::<Metadata>();
+//! ```
 
 #[macro_use]
 pub(crate) mod macros;
+#[rustfmt::skip]
 pub mod attr;
-pub mod core;
-mod test;
+pub mod traits;
 
 pub use attr::*;
-pub use core::*;
+pub use traits::*;
--- bascet-core/src/cell/attr.rs
+++ bascet-core/src/cell/attr.rs
@@ -9,6 +9,7 @@ impl_attrs!(
     ManagedRef
 );
 
+#[rustfmt::skip]
 impl_tuple_provide!(Arg1, Arg2);
 impl_tuple_provide!(Arg1, Arg2, Arg3);
 impl_tuple_provide!(Arg1, Arg2, Arg3, Arg4);
@@ -20,16 +21,7 @@ impl_tuple_provide!(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9);
 impl_tuple_provide!(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10);
 impl_tuple_provide!(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11);
 impl_tuple_provide!(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12);
-impl_tuple_provide!(
-    Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13
-);
-impl_tuple_provide!(
-    Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14
-);
-impl_tuple_provide!(
-    Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15
-);
-impl_tuple_provide!(
-    Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15,
-    Arg16
-);
+impl_tuple_provide!(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13);
+impl_tuple_provide!(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14);
+impl_tuple_provide!(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15);
+impl_tuple_provide!(Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9, Arg10, Arg11, Arg12, Arg13, Arg14, Arg15, Arg16);
