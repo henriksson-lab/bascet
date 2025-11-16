cf5e5c067525b63bcc9c3755ee4c25aadcd2fb4d
Julian Dicken <julian@dicken.de>
2025-11-16 15:57:13 +0100

    .

--- bascet-core/src/lib.rs
+++ bascet-core/src/lib.rs
@@ -1,10 +1,12 @@
 pub mod attr;
 pub mod composite;
 pub mod stream;
+pub mod utils;
 
 pub use attr::*;
 pub use composite::*;
 pub use stream::*;
+pub use utils::*;
 
 pub use bascet_apply::apply_selected;
 pub use bascet_derive::Composite;
