958b3906aa0c5f6c990b15f451e2b6539a22a736
Julian Dicken <julian@dicken.de>
2026-01-22 17:42:06 +0100

    .

--- bascet-io/src/codec/bbgz/block.rs
+++ bascet-io/src/codec/bbgz/block.rs
@@ -50,26 +50,6 @@ impl<'a> BBGZWriteBlock<'a> {
 
 impl<'a> std::io::Write for BBGZWriteBlock<'a> {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        // NOTE hsize assumes the entire data is written as one incompressible block
-        let hsize = self.inner_raw_bytes_written
-            + self.inner_header.size()
-            + SIZEOF_MARKER_DEFLATE_ALIGN_BYTESusize
-            + BBGZTrailer::SSIZE;
-
-        if buf.len() + hsize > MAX_SIZEOF_BLOCKusize {
-            let new_raw = self.inner_compressor.alloc_raw();
-            let mut send_raw = std::mem::replace(&mut self.inner_raw, new_raw);
-            unsafe {
-                send_raw = send_raw.truncate(self.inner_raw_bytes_written);
-                let send_job = BBGZCompressionJob {
-                    header: self.inner_header.clone(),
-                    raw: send_raw,
-                };
-                self.inner_compressor.submit_compress(send_job);
-            }
-            self.inner_raw_bytes_written = 0;
-        }
-
         let raw_buf = self.inner_raw.as_mut_slice();
         unsafe {
             let raw_buf_ptr = raw_buf.as_mut_ptr().add(self.inner_raw_bytes_written);
