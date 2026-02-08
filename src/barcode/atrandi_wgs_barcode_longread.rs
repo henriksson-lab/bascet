d0f4c5e5924bd6a6a0a172506bb19543c5c201ed
Johan Henriksson <mahogny@areta.org>
2026-02-08 16:53:24 +0100

    atrandi WGS for longread, preparing to integrate in getraw

--- src/barcode/atrandi_wgs_barcode_longread.rs
+++ src/barcode/atrandi_wgs_barcode_longread.rs
@@ -34,7 +34,7 @@ impl DebarcodeAtrandiWGSChemistryLongread {
         result.barcode.pools[0].pos_anchor = (8 + 4) * 3;
         result.barcode.pools[0].pos_rel_anchor = vec![0, 1];
 
-        result.barcode.trim_bcread_len=8+4+8+4+8+4+8+1; //8 barcodes, 3 spacers, and 1 to account for ligation
+        //result.barcode.trim_bcread_len=8+4+8+4+8+4+8+1; //8 barcodes, 3 spacers, and 1 to account for ligation
 
         result
     }
@@ -67,38 +67,112 @@ impl crate::barcode::Chemistry for DebarcodeAtrandiWGSChemistryLongread {
         r2_seq: &'a [u8],
         r2_qual: &'a [u8],
     ) -> (u32, crate::common::ReadPair<'a>) {
-        //Detect barcode, which here is in R2
-        let total_distance_cutoff = 4;
-        let part_distance_cutoff = 1;
+
+
+        // /husky/henriksson/atrandi/rawdata/cleanbar_longread
+        // zcat SRR31758484.fastq.gz  | head -n 100 | grep TTCCGATCT --color 
+        // Example index primer p7: UDP0005_p7	CAAGCAGAAGACGGCATACGAGAT TAATCTCGTC GTGACTGGAGTTCAGACGTGTGCTCTT
+        // generic P5	AATGATACGGCGACCACCGAGATCT    ACACTCTTTCCCTAC ACGAC
+
+        //zcat SRR31758484.fastq.gz | grep AGATCGGAAGAGCACACGTCT
+        //this is the end. but the end of this BC might be truncated
+
+        // Adapters confirmed in cleanbar fig https://academic.oup.com/view-large/figure/530094580/ycaf134f2.tif  
+        // https://pubmed.ncbi.nlm.nih.gov/40860566/
+        // zcat SRR31758484.fastq.gz | grep CTTCCGATCT........AGGA........ACTC........AAGG........T
+        // zcat SRR31758484.fastq.gz | grep ........AGGA........ACTC........AAGG........T
+        // ==> reverse complement
+        // zcat SRR31758484.fastq.gz | grep T........CCTT........GAGT........TCCT........ # AGATC
+        // these fragments are rare in cleanbar. depends on how longreads where made .. methods are unclear. possible that amplicons were made, all in the same orientation
+
+        // zcat SRR31758484.fastq.gz | grep CTTCCGATCT........AGGA........ACTC........AAGG........T
+        // 83 results (1000 possible)
+        // zcat SRR31758484.fastq.gz | grep ........AGGA........ACTC........AAGG........T
+        // 112 results (1000 possible)
+        // From https://academic.oup.com/ismecommun/article/5/1/ycaf134/8220722 , it appears that no final PCR was done.
+        // This goes against the Atrandi manual recommendation and thus many failed adapters are expected.
+        // As longread should not be done in this manner and we should not attempt to support non-recommended solutions.
+
+        // We can scan for up to 13bp of just linkers
+        // Prob of random hit: 13bp (1/4)**13 = 1.490116e-08
+        // This is highly unlikely and to ensure we do not miss reads, we need to do some fuzzy searching. Already scanning for AGGA...ACTC would be sufficient (p=1.5-e5; 0.15 such matches in 10kb)
+
+        
+        // Scan for the position of barcode
+        let scan_bc = b"\0\0\0\0AGGAACTCAAGG"; //4x u32
+        let scan_bc_u64 = copy_u8_to_u64(scan_bc);
+        let bc_len = 8+4+8+4+8+4+8+1;
+
+        'linker_scan: for (curpos,inp) in r1_seq.windows(bc_len).enumerate() { 
+
+            let ad1 = &inp[(0+8 )..(4+8 )];
+            let ad2 = &inp[(0+20)..(4+20)];
+            let ad3 = &inp[(0+32)..(4+32)];
+
+            let ad1 = copy_u8_to_u32(ad1) as u64;
+            let ad2 = copy_u8_to_u32(ad2) as u64;
+            let ad3 = copy_u8_to_u32(ad3) as u64;
+
+            let all_ad = ad1<<16 | ad2<<8 | ad3;
+            let adapter_score = hamming_u64(all_ad, scan_bc_u64);
+
+            //Expected similarity: letters A-T are in the range dec 65 .. dec 74 ; 1000001 .. 1001010   so there are 4 bits that can change
+            //Least similarity (ish): 4*3+8 = 20. Highest: 32
+            //A cutoff at >=29 gives decent chance of a barcode being present
+
+            if adapter_score >= 29 {
+
+                //Detect barcode at this position
+                let total_distance_cutoff = 4;
+                let part_distance_cutoff = 1;
+                let (bc, bc_score) = self.barcode.detect_barcode(inp, true, total_distance_cutoff, part_distance_cutoff);
+
+                //Trim the remaining read if the barcode is good enough
+                if bc_score >= 0 {
+                    //TODO detect end of the read
+                    //Note that the final barcode can be /very/ incomplete the way it was done previously, https://academic.oup.com/ismecommun/article/5/1/ycaf134/8220722?login=false
+                    //This also makes scanning for the end slow and error prone. Instead, we simply trim the final 80 bp, which is margin beyond the expected adapter size (55bp)
+
+                    //Ensure fragment is long enough
+                    let end_trim = 80;
+                    if r1_seq.len() < end_trim {
+                        break 'linker_scan;
+                    }
+
+                    //
+                    let seq_from = r1_seq.len().min(curpos+bc_len);
+                    let seq_to = r1_seq.len()-end_trim;
+                    if seq_to < seq_from {
+                        break 'linker_scan;
+                    }
+
+                    return (
+                        bc, 
+                        ReadPair {
+                            r1: &r1_seq[seq_from..seq_to],
+                            r2: &r2_seq,
+                            q1: &r1_qual[seq_from..seq_to],
+                            q2: &r2_qual,
+                            umi: &[],
+                        },
+                    )
+
+                } else if adapter_score==32 {
+                    //If we found a perfect linker hit then we are done, even if the barcode was bad. Otherwise we should continue scanning
+                    break 'linker_scan;
+                }
+
+            }
 
 
 
-
-        //TODO find position of barcode
-
+        }
 
 
         //TODO discard if not enough of read left
 
 
 
-        //Detect the barcode
-        let (bc, score) =
-            self.barcode
-                .detect_barcode(r2_seq, true, total_distance_cutoff, part_distance_cutoff);
-
-        //Trim the remaining read if the barcode is good enough
-        if score >= 0 {
-
-
-
-            //TODO detect end of the read
-
-
-
-        }
-
-            
 
         return (
             u32::MAX, //=Discard
@@ -113,4 +187,32 @@ impl crate::barcode::Chemistry for DebarcodeAtrandiWGSChemistryLongread {
         
     }
 
+}
+
+
+/// Copy a list of u8 into a u32 (almost a transmute).
+/// This function is as fast as if the size was explicitly given; size is likely added on top during inlining
+#[inline(always)]
+fn copy_u8_to_u32(out: &[u8]) -> u32 {
+    let mut arr = [0u8; 4];
+    arr.copy_from_slice(&out[0..4]);
+    let toret = u32::from_ne_bytes(arr);
+    toret
+}
+
+
+
+/// Copy a list of u8 into a u64 (almost a transmute).
+/// This function is as fast as if the size was explicitly given; size is likely added on top during inlining
+#[inline(always)]
+fn copy_u8_to_u64(out: &[u8]) -> u64 {
+    let mut arr = [0u8; 8];
+    arr.copy_from_slice(&out[0..8]);
+    let toret = u64::from_ne_bytes(arr);
+    toret
+}
+
+#[inline(always)]
+fn hamming_u64(a: u64, b: u64) -> u8 {
+    (a ^ b).count_ones() as u8
 }
